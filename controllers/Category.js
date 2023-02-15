9db45baa993958f9bb151285f391a2c7563cd5da
labaynecss <labaynecss@gmail.com>
2023-02-16 06:19:50 +0800

    update url

--- controllers/Category.js
+++ controllers/Category.js
@@ -1,5 +1,5 @@
 const { validationResult } = require('express-validator');
-const CategoryModel = require('../models/CategoryModel');
+const CatgoryModel = require('../models/CategoryModel');
 
 class Category {
   async create(req, res) {
